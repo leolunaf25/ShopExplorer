033b67bdda0c8a5cc549fd189811a7a4f3a33eae
leolunaf25 <lunalunaf25@gmail.com>
2025-01-15 23:56:50 -0600

    add navigation controller

--- app/src/main/java/com/lunatcoms/shopexplorer/presentation/ui/activity/MainActivity.kt
+++ app/src/main/java/com/lunatcoms/shopexplorer/presentation/ui/activity/MainActivity.kt
@@ -2,16 +2,31 @@ package com.lunatcoms.shopexplorer.presentation.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.lunatcoms.shopexplorer.R
 import com.lunatcoms.shopexplorer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initUI()
+    }
+
+    private fun initUI() {
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomNavView.setupWithNavController(navController)
     }
 }
